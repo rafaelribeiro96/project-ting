afd40d191372996c126d47bc76776783437ac29e
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-05-13 13:30:24 -0300

    add file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+def importador_de_txt(caminho_arquivo: str):
+    try:
+        # Verificando se a extensão do arquivo é ".txt"
+        if not caminho_arquivo.endswith(".txt"):
+            raise TypeError
+        
+        # Abrindo o arquivo e armazenando cada linha em uma lista
+        with open(caminho_arquivo) as arquivo:
+            lista_linhas = [linha_arquivo.rstrip("\n") for linha_arquivo in arquivo]
+            
+            # Retornando a lista de linhas
+            return lista_linhas
+    
+    # Tratando o erro caso o arquivo não seja encontrado
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {caminho_arquivo} não encontrado\n")
+    
+    # Tratando o erro caso o formato do arquivo não seja válido
+    except TypeError:
+        sys.stderr.write("Formato inválido")
