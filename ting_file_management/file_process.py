0a5d74da40f4f4f140f3a6c784dc999d241f510a
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-05-13 13:58:59 -0300

    refactor file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,48 +1,42 @@
 import sys
-from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
 
 
-def process(path_file, instancia_fila: Queue):
-    # Importando o arquivo utilizando a função "txt_importer"
-    lista_linhas = txt_importer(path_file)
+def process(file, inst: Queue):
+    arr = []
 
-    # Criando uma lista para os nomes dos arquivos
-    lista_arqui = []
+    get_file = txt_importer(file)
 
-    for i in range(len(instancia_fila)):
-        if not instancia_fila.search(i)["nome_do_arquivo"]:
+    for length in range(len(inst)):
+        if not inst.search(length)["nome_do_arquivo"]:
             return None
-        lista_arqui.append(instancia_fila.search(i)["nome_do_arquivo"])
+        arr.append(inst.search(length)["nome_do_arquivo"])
 
-    # Adicionando as informações do arquivo à fila caso ele não exista
-    if path_file not in lista_arqui:
-        novo_arquivo = {
-            "nome_do_arquivo": path_file,
-            "qtd_linhas": len(lista_linhas),
-            "linhas_do_arquivo": lista_linhas,
+    if file not in arr:
+        new_info = {
+            "nome_do_arquivo": file,
+            "qtd_linhas": len(get_file),
+            "linhas_do_arquivo": get_file,
         }
-        instancia_fila.enqueue(novo_arquivo)
-        sys.stdout.write(str(novo_arquivo))
+        inst.enqueue(new_info)
+        sys.stdout.write(str(new_info))
 
 
-def remove(instancia_fila: Queue):
+def file_metadata(inst: Queue, pos):
     try:
-        if len(instancia_fila) > 0 or instancia_fila:
-            # Removendo o primeiro elemento da fila
-            arquivo_removido = instancia_fila.dequeue()
-            path_file = arquivo_removido["nome_do_arquivo"]
-            sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
-        # Retornando uma mensagem caso a fila esteja vazia
+        f_result = inst.search(pos)
+        sys.stdout.write(str(f_result))
+    except IndexError:
+        sys.stderr.write("Posição inválida")
+
+
+def remove(inst: Queue):
+    try:
+        if len(inst) > 0 or inst:
+            get_file = inst.dequeue()
+            f_path = get_file["nome_do_arquivo"]
+            sys.stdout.write(f"Arquivo {f_path} removido com sucesso\n")
         sys.stdout.write("Não há elementos\n")
     except TypeError:
         print(TypeError)
-
-
-def file_metadata(instancia_fila: Queue, posicao):
-    try:
-        # Buscando as informações do arquivo na posição indicada
-        resultado_busca = instancia_fila.search(posicao)
-        sys.stdout.write(str(resultado_busca))
-    except IndexError:
-        sys.stderr.write("Posição inválida")
