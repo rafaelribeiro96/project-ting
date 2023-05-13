be936cdea2613e451bdac80bd1ae5bafd2bf423b
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-05-13 14:24:21 -0300

    fix queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,18 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__queue = []
+        self.queue = list()
 
     def __len__(self):
-        return len(self.__queue)
+        return len(self.queue)
 
     def enqueue(self, value):
-        self.__queue.append(value)
+        self.queue.append(value)
 
     def dequeue(self):
-        self.__queue.pop(0)
+        if len(self.queue) == 0:
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        if not (0 >= index <= len(self.__queue)):
-            raise IndexError("Índice Inválido ou Inexistente")
-        return self.__queue[index]
+        if 0 <= index <= len(self.queue) - 1:
+            return self.queue[index]
+        raise IndexError("Índice Inválido ou Inexistente")
